9f3a0ccd2ad08b09e82cdbcbb6415aa498cdf09f
Rajashekar Chintalapati <rchintalapati@walmartlabs.com>
2017-12-18 21:12:22 -0800

    feat: Added text input and submit button to new deck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,85 @@
 import React, { Component } from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TextInput,
+        TouchableOpacity, StyleSheet, Platform} from 'react-native'
+import { purple, white } from '../utils/colors'
 
 class NewDeck extends Component {
+    state = {
+        deckTitle : "Deck Title"
+    }
+
+    submit = () => {
+        console.log("submit button is clicked with value",this.state.deckTitle)
+    }
+
     render() {
         return (
             <View>
-                <Text>New Deck View</Text>
+                <Text style={styles.deckQtn}>What is the title of your new deck? </Text>
+                <View style={{paddingTop: 10}}>
+                    <TextInput 
+                        placeholder={this.state.deckTitle}
+                        style={styles.deckTitle}
+                        onChangeText={(text) => this.setState({deckTitle: text})}
+                    />
+                </View>
+                <View style={{paddingTop: 10}}>
+                    <SubmitBtn onPress={this.submit}/>
+                </View>
             </View>
         )
     }
 }
+
+function SubmitBtn({ onPress }) {
+    return (
+        <TouchableOpacity 
+            style={Platform === 'ios'?styles.iosSubmitBtn:styles.AndroidSubmitBtn}
+            onPress={onPress}>
+            <Text style={styles.submitBtnText}>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+  deckQtn: {
+    paddingTop: 30,
+    textAlign: 'center',
+    fontSize: 52,
+  },
+  deckTitle: {
+    height: 40,
+    width: 300,
+    borderColor: purple,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40,
+  },
+  AndroidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center',
+  },
+})
 
 export default NewDeck
---
+++ utils/api.js
@@ -0,0 +1,15 @@
+import { AsyncStorage } from 'react-native'
+
+export const DECK_STORAGE_KEY = 'UdaciCards:Decks'
+
+function saveDeckTitle(title) {
+}
+
+function getDeck(id) {
+}
+
+function getDecks() {
+}
+
+function addCardToDeck(title,card) {
+}
